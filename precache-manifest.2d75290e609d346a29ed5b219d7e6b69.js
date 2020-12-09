1d4438a4c12fb464e205e660fb2c54ae6c185880
JinalBhanushali0628 <jinalbhanushali0628@gmail.com>
2020-12-08 21:38:46 -0500

    Updates

---
+++ precache-manifest.2d75290e609d346a29ed5b219d7e6b69.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f978c8308c5375f84523ec8db3e3fc",
+    "url": "/iei-web/index.html"
+  },
+  {
+    "revision": "08ae9be925eb624b4db4",
+    "url": "/iei-web/static/css/main.45b67897.chunk.css"
+  },
+  {
+    "revision": "0fa2d4cebf8c7dbdf341",
+    "url": "/iei-web/static/js/2.92be2ad5.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/iei-web/static/js/2.92be2ad5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08ae9be925eb624b4db4",
+    "url": "/iei-web/static/js/main.6a0a323a.chunk.js"
+  },
+  {
+    "revision": "6a194c6ecf5db4044618",
+    "url": "/iei-web/static/js/runtime-main.8409f512.js"
+  },
+  {
+    "revision": "b10969c6615897d6ac80df7439475c06",
+    "url": "/iei-web/static/media/event2.b10969c6.jpg"
+  },
+  {
+    "revision": "70f976fa046a4ca9ae0cddebd4edac21",
+    "url": "/iei-web/static/media/happy.70f976fa.gif"
+  },
+  {
+    "revision": "ce672444c010099c4eac4ef248874cb9",
+    "url": "/iei-web/static/media/iei_logo.ce672444.png"
+  },
+  {
+    "revision": "e0ea88c5db6ffde45479a44baa87da20",
+    "url": "/iei-web/static/media/join.e0ea88c5.gif"
+  }
+]);
